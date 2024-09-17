3d3305e37ab88f36d74411757e49f7adc9360161
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-09-17 15:33:31 +0200

    added basic html and css to roughly add all the html elements that need to be present

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let numberOne = 0
+let numberTwo = 0
+let mutator = ''
+
+/*.addEventListener('click',() => {
+    calculate(numberOne,numberTwo,mutator)
+}); */
 function calculate(firstNumber, secondNumber, operator){
     if (!secondNumber) return firstNumber;
 
