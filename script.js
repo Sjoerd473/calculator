ea0d59e24e7530f3784a6cabab250cf0018b361f
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-09-17 15:05:03 +0200

    added function calculate, will handle calculating the values received through inputs

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('test')
+function calculate(firstNumber, secondNumber, operator){
+    if (!secondNumber) return firstNumber;
+
+    const effect = operator;
+        switch (effect){
+            case '+':
+            return firstNumber + secondNumber;
+            
+            case '-':
+            return firstNumber - secondNumber;   
+
+            case '*':
+            return firstNumber * secondNumber;
+
+            case '/':
+            return firstNumber / secondNumber;
+        }
+}
